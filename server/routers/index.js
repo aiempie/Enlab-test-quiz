82649f4f9b32168661b06ecf8bfde92193b4e536
khanhtn <khanhtnfptdn@gmail.com>
2023-06-10 19:54:34 +0700

    fix hello world

--- server/routers/index.js
+++ server/routers/index.js
@@ -6,14 +6,14 @@ const setQuizRouters = require("./setQuiz/setQuizRouter");
 
 const routes = (app) => {
   const apiRoute = (routeName) => `/api/v1/${routeName}`;
-  app.use("/", (req, res) => {
-    res.send("Hello World");
-  });
   app.use(apiRoute("auth"), authRouters);
   app.use(apiRoute("question"), questionRouters);
   app.use(apiRoute("category"), categoryRouters);
   app.use(apiRoute("diffculty"), difficultyRouters);
   app.use(apiRoute("set-quiz"), setQuizRouters);
+  app.use("/", (req, res) => {
+    res.send("Hello World");
+  });
 };
 
 module.exports = routes;
